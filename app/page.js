ffe2487a3a362eda47b0a62fe973662ffc7f41ba
cotlevercom-ship-it <cotlever.com@gmail.com>
2026-07-07 23:06:53 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,6 +2,8 @@ import Hero from '@/components/Hero'
 import ShopSection from '@/components/ShopSection'
 import Footer from '@/components/Footer'
 
+export const dynamic = 'force-dynamic'
+
 export default function Home() {
   return (
     <div style={{ minHeight: '100vh', background: '#f5f5f5' }}>
