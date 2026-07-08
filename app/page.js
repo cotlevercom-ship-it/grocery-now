ceadc32082d4ffa2889b1aa958e918e49d047a89
cotlevercom-ship-it <cotlever.com@gmail.com>
2026-07-07 22:42:02 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,22 +1,11 @@
-import { supabaseFetch } from '@/lib/supabase'
 import Hero from '@/components/Hero'
-import AreaSection from '@/components/AreaSection'
 import ShopSection from '@/components/ShopSection'
 import Footer from '@/components/Footer'
 
-export const dynamic = 'force-dynamic'
-
-export default async function Home() {
-  let areas = []
-  try {
-    areas = await supabaseFetch('areas?select=*&is_active=eq.true&order=name')
-  } catch (e) {
-    console.error(e)
-  }
+export default function Home() {
   return (
     <div style={{ minHeight: '100vh', background: '#f5f5f5' }}>
       <Hero />
-      <AreaSection areas={areas} />
       <ShopSection />
       <Footer />
     </div>
