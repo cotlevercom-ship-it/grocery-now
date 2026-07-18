149e719cc77dcbe65826dbdcd1ae5b82a16a9582
cotlevercom-ship-it <cotlever.com@gmail.com>
2026-07-19 02:33:49 +0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import Hero from '@/components/Hero'
+import AreaSectionWrapper from '@/components/AreaSectionWrapper'
 import ShopSection from '@/components/ShopSection'
 import Footer from '@/components/Footer'
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <div style={{ minHeight: '100vh', background: '#f5f5f5' }}>
       <Hero />
+      <AreaSectionWrapper />
       <ShopSection />
       <Footer />
     </div>
