e6ba2e52ac63dc4d1cf4749881d6555f47e51152
cotlevercom-ship-it <cotlever.com@gmail.com>
2026-07-24 01:36:02 +0600

    Update layout.js

--- app/seller/layout.js
+++ app/seller/layout.js
@@ -1,25 +1,3 @@
-import '../globals.css'
-import Navbar from '@/components/Navbar'
-
-
-export const metadata = {
-  title: 'GroceryNow',
-  description: 'আপনার এলাকার সেরা গ্রোসারি',
-}
-
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="bn">
-      <head>
-        <link href="https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@400;500;600;700&display=swap" rel="stylesheet" />
-      </head>
-      <body>
-        <div className="app-container">
-          <Navbar />
-          {children}
-        </div>
-      </body>
-    </html>
-  )
+export default function SellerLayout({ children }) {
+  return children
 }
