ad4e713789a425b596646f121d2a1e645ab0fc11
Rfin <rfin@cotlever.com>
2026-08-04 15:53:06 +0000

    Remove Trust badges section from homepage

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 import Hero from '@/components/Hero'
 import HomeSearch from '@/components/HomeSearch'
-import TrustBadges from '@/components/TrustBadges'
 import PopularProducts from '@/components/PopularProducts'
 import ShopSection from '@/components/ShopSection'
 import HomeTabs from '@/components/HomeTabs'
@@ -13,7 +12,6 @@ export default function Home() {
     <div style={{ minHeight: '100vh', background: '#f5f5f5' }}>
       <Hero />
       <HomeSearch />
-      <TrustBadges />
       <HomeTabs productsSlot={<PopularProducts />} shopsSlot={<ShopSection />} />
       <Footer />
     </div>
