5af8bc26b9310c2b49feba1e6919491d3fb67c3b
Rfin <rfin@cotlever.com>
2026-08-24 00:29:09 +0000

    Escape < in article JSON-LD to prevent a </script> breakout injection from article title/excerpt content

--- app/resources/[slug]/page.js
+++ app/resources/[slug]/page.js
@@ -47,7 +47,7 @@ export default async function ArticlePage({ params }) {
       {jsonLd && (
         <script
           type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd).replace(/</g, '\\u003c') }}
         />
       )}
       <ArticleClient />
